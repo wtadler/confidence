e5b70a146fa41fb32c8dd75fb45ca300b6d75403
GCB111 <gcb280@nyu.edu>
2016-04-07 23:23:21 -0400

    Psybayes_Plot update
    Fixed NaN issue in bin combination

--- helper_functions/psybayes_plot.m
+++ helper_functions/psybayes_plot.m
@@ -1,4 +1,4 @@
-load('/Users/bluec/Documents/GitHub/confidence/data/backup/GB_notrain_final.mat')
+load('/Users/bluec/Documents/GitHub/confidence/data/backup/willstaircase3.mat')
 st(1) = psybayes_struct.valid;
 st(2) = psybayes_struct.neutral;
 st(3) = psybayes_struct.invalid;
@@ -53,20 +53,34 @@ for i = 1:nCurves
     [bincounts,ind] = histc(contrasts,binranges);
     outcome = tab.data(:,2);
 
-%Most efficient version; plots error bars for each bin:
-%std = (max-min)/6
-    for i = 1:nBins
-        y(i) = mean(outcome(ind==i));
+%Most computationally efficient version; plots error bars for each bin:
+    yy= zeros(1,nBins-1);
+    plotpoints = zeros(1,nBins-1);
+    %Plot each of the first four bins individually:
+    for i = 1:length(yy)
+        yy(i) = mean(outcome(ind==i));
+        plotpoints(i) = (binranges(i)+binranges(i+1))/2;
+        a(i) = sum(outcome(ind==i)==1);
+        b(i) = sum(outcome(ind==i)==0);
     end
-    miss = zeros(1,6);
-    var = zeros(1,6);
-    EB = zeros(1,6);
-    for i = 1:length(y)
-        miss(i) = 1-y(i);
-        var(i) = (y(i)*miss(i))/((y(i)+miss(i))^2*(y(i)+miss(i)+1));% variance calc for each binned point
-        EB(i) = sqrt(var(i)); %standard deviations (used as the length of each error bar)
+   %Combine values for the final two bins:
+   if isnan(yy(end)) == 1
+       yy(end) = mean(outcome(ind==nBins)); %Resets NaN resulting from mean calculation for an empty bin
+       plotpoints(end) = binranges(end); %Reset the plotting point to reflect only the bin with data
+   else
+       yy(end) = (yy(end)+mean(outcome(ind==nBins)))/2;
+   end
+    a(end) = (a(end)+sum(outcome(ind==nBins)==1));
+    b(end) = (b(end)+sum(outcome(ind==nBins)==0));
+   %Get variance and SD (error bar length)
+    var = zeros(1,length(yy));
+    EB = zeros(1,length(yy));
+    for i = 1:length(yy)
+        var(i) = (a(i)*b(i))/((a(i)+b(i))^2*(a(i)+b(i)+1));% variance calc for each binned point
+        EB(i) = sqrt(var(i)); %standard deviations (used as the length of each error bar)  
     end
-    p = errorbar(binranges, y, EB, '.', 'color', c, 'markersize', 12);
+    %And finally plot everything:
+    p = errorbar(plotpoints, yy, EB, '.', 'color', c, 'markersize', 12);
 
 %Loop version (less efficient, but plots size-dependent points for each bin):    
 %    x(1:nBins-1) = binranges(1:end-1) + diff(binranges)/2;
@@ -80,10 +94,12 @@ for i = 1:nCurves
 %    end
 
 %To plot trials at the top/bottom of the screen (as opposed or in addition to binned data):
-    yy = tab.data(:, 2);
+    y = tab.data(:, 2);
+    y(y==1) = yl(2);
+    y(y==0) = yl(1);
     n = length(contrasts);
     noise = .005;
-    p = plot(contrasts+noise*randn(n, 1), yy+noise*randn(n, 1), '.', 'color', c, 'markersize', 20)
+    p = plot(contrasts+noise*randn(n, 1), y+noise*randn(n, 1), '.', 'color', c, 'markersize', 20)
     
 end
 
