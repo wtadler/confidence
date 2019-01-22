98e6f81e4518bb6705c4118c040e293296a3fe69
Will Adler <will@wtadler.com>
2019-01-22 15:39:55 -0500

    compile reliability exp 1 data to big CSV

--- human_data/README.md
+++ human_data/README.md
@@ -1,3 +1,23 @@
-The three reliability experiments are described in "[Comparing Bayesian and non-Bayesian accounts of human confidence reports](https://www.biorxiv.org/content/early/2018/01/29/093203)."
+This folder contains `.mat` files for each session from each subject, from four experiments.
 
-The attention experiment is described in "[Humans flexibly incorporate attention-dependent uncertainty into perceptual decisions and confidence](https://www.biorxiv.org/content/early/2017/08/10/175075)."
+The attention experiment is described in [Humans incorporate attention-dependent uncertainty into perceptual decisions and confidence](http://www.pnas.org/content/early/2018/10/05/1717720115) [[pdf]](http://www.wtadler.com/papers/Denison2018.pdf).
+
+The three reliability experiments are described in [Comparing Bayesian and non-Bayesian accounts of human confidence reports](https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1006572) [[pdf]](http://www.wtadler.com/papers/Adler2018b.pdf). The file [`reliability_exp1.csv`](reliability_exp1.csv) contains all data for Experiment 1 in this paper. The important columns in this spreadsheet are:
+
+- `subject_name`
+- `task`
+  - A: Task in which categories have different means, same SDs
+  - B: Task in which categories have same means, different SDs
+- `block_type`
+  - Training: Category training (high stimulus reliability, correctness feedback)
+  - Test: Test block (variable stimulus reliability, no trial-to-trial correctness feedback)
+- `stim_category` Binary category (-1 or 1) specifying a stimulus distribution
+- `stim_type`
+  - grate: Drifting oriented Gabor
+  - ellipse: Oriented ellipse
+- `stim_reliability` Stimulus contrast (for `stim_type` Gabor) or eccentricity (for `stim_type` ellipse)
+- `stim_orientation` Stimulus orientation (degrees)
+- `resp_buttonid` Subject response (integers -4 through 4, except 0, with -4 indicating "high confidence cat. -1" and 4 indicating "high confidence cat. 1")
+- `resp_category` Category choice (-1 or 1)
+- `resp_confidence` Confidence report (1 through 4)
+- `resp_rt` Reaction time (seconds)
---
+++ human_data/subject_data_to_csv.py
@@ -0,0 +1,135 @@
+import numpy as np
+import os
+import datetime
+import re
+import pandas as pd
+from scipy import io
+
+### https://stackoverflow.com/questions/7008608/scipy-io-loadmat-nested-structures-i-e-dictionaries
+def loadmat(filename):
+    '''
+    this function should be called instead of direct spio.loadmat
+    as it cures the problem of not properly recovering python dictionaries
+    from mat files. It calls the function check keys to cure all entries
+    which are still mat-objects
+    '''
+    data = io.loadmat(filename, struct_as_record=False, squeeze_me=True)
+    return _check_keys(data)
+
+def _check_keys(dict):
+    '''
+    checks if entries in dictionary are mat-objects. If yes
+    todict is called to change them to nested dictionaries
+    '''
+    for key in dict:
+        if isinstance(dict[key], io.matlab.mio5_params.mat_struct):
+            dict[key] = _todict(dict[key])
+    return dict        
+
+def _todict(matobj):
+    '''
+    A recursive function which constructs from matobjects nested dictionaries
+    '''
+    dict = {}
+    for strg in matobj._fieldnames:
+        elem = matobj.__dict__[strg]
+        if isinstance(elem, io.matlab.mio5_params.mat_struct):
+            dict[strg] = _todict(elem)
+        else:
+            dict[strg] = elem
+    return dict
+###
+
+class Vividict(dict):
+    def __missing__(self, key):
+        value = self[key] = type(self)() # retain local pointer to value
+        return value        
+        
+
+block_types = ['Test','Training']
+
+def oldcat2newcat(x):
+    # turns 1 and 2 into -1 and 1
+    return 2 * np.array(x,np.int8) - 3
+
+#%%
+experiments = Vividict({
+               'reliability_exp1': {
+                   'A':'reliability_exp1/taskA',
+                   'B':'reliability_exp1/taskB'}})
+
+# experiments = Vividict({'attention': {
+#                    'B':'attention'},
+#                'reliability_exp1': {
+#                    'A':'reliability_exp1/taskA',
+#                    'B':'reliability_exp1/taskB'},
+#                'reliability_exp2': {
+#                    'A':'reliability_exp2/taskA',
+#                    'B':'reliability_exp2/taskB'},
+#                'reliability_exp3': {
+#                    'B':'reliability_exp3'}
+#                })
+
+for experiment in experiments:
+    experiments[experiment]['subjects'] = Vividict()
+    for task, taskdir in [i for i in experiments[experiment].items() if i[0]!='subjects']:
+        for file in os.listdir(taskdir):
+            if file.endswith('.mat'):
+                name, date, time = re.split('_',file)[0:3]
+
+                if name not in experiments[experiment]['subjects']:
+                    experiments[experiment]['subjects'][name]['df'] = pd.DataFrame()
+
+                session_start_time = datetime.datetime(int(date[0:4]),int(date[4:6]),int(date[6:8]),int(time[0:2]),int(time[2:4]),min(59,int(time[4:6])))
+                mat = loadmat(taskdir+'/'+file)
+                stim_type = mat['P']['stim_type']
+
+                for block_type in block_types:
+                    tmpdata = mat[block_type]
+                    stim_dict = tmpdata['R']
+                    resp_dict = tmpdata['responses']
+                    
+                    for i, object in enumerate(resp_dict):
+                        resp_dict[i] = _todict(object)
+                    for block_no in range(0,len(tmpdata['R']['draws'])):
+                        # for blocks with only one section, wrap in a list to make the indexing work better
+                        if np.array(stim_dict['draws'][block_no]).ndim==1:
+                            for d in ['draws', 'sigma', 'phase', 'trial_order']:
+                                stim_dict[d][block_no]=[stim_dict[d][block_no]]
+                            for d in ['tf', 'c', 'conf', 'rt']:
+                                resp_dict[block_no][d]=[resp_dict[block_no][d]]
+                            
+                            
+                        for section_no in range(0,len(tmpdata['R']['draws'][block_no])):
+                            stim_category = oldcat2newcat(stim_dict['trial_order'][block_no][section_no])
+                            resp_category = oldcat2newcat(resp_dict[block_no]['c'][section_no])
+
+                            tmpdf = pd.DataFrame({'stim_orientation': stim_dict['draws'][block_no][section_no],
+                                                  'stim_reliability': stim_dict['sigma'][block_no][section_no],
+                                                  'stim_phase': stim_dict['phase'][block_no][section_no],
+                                                  'stim_category': stim_category,
+                                                  'stim_type': stim_type,
+                                                  'resp_confidence': resp_dict[block_no]['conf'][section_no],
+                                                  'resp_category': resp_category,
+                                                  'resp_buttonid': resp_category*resp_dict[block_no]['conf'][section_no],
+                                                  'resp_correct': resp_dict[block_no]['tf'][section_no],
+                                                  'resp_rt': resp_dict[block_no]['rt'][section_no],
+                                                  'block_no': block_no,
+                                                  'section_no': section_no,
+                                                  'task': task,
+                                                  'block_type': block_type,
+                                                  'session_start_time': session_start_time,
+                                                  })
+                            experiments[experiment]['subjects'][name]['df'] = pd.concat([experiments[experiment]['subjects'][name]['df'], tmpdf], ignore_index = True)
+                            experiments[experiment]['subjects'][name]['df']['subject_name'] = name
+
+for experiment in experiments:
+    dfs = [i[1]['df'] for i in experiments[experiment]['subjects'].items()]
+    sort_cols = ['subject_name', 'session_start_time', 'task', 'block_type', 'block_no', 'section_no']
+    ascending = [False if i=='block_type' else True for i in sort_cols]
+
+    cols = ['subject_name', 'session_start_time', 'task', 'block_type', 'block_no', 'section_no', 'stim_type', 'stim_category', 'stim_reliability', 'stim_orientation', 'stim_phase', 'resp_buttonid', 'resp_category', 'resp_confidence', 'resp_rt', 'resp_correct']
+
+    all = pd.concat(dfs, ignore_index=True)
+    all = all.sort_values(sort_cols, ascending=ascending)[cols]
+    all.to_csv(f'{experiment}.csv', index=False)
